9e8544cecd494cc144e188a3d7e06f3c3c49645b
vansh13code <vanshbatra734@gmail.com>
2026-04-06 13:00:35 +0530

    new things

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import asyncio
+import aiohttp
 import csv
 import requests
 from flask import Flask,jsonify,request
@@ -18,11 +20,12 @@ def get_weather():
             "temperature":data["main"]["temp"],
             #"description":data["weather"][0]["description"],
             "humidity":data["main"]["humidity"],
-            "Condition":data["weather"][0]["main"]
+            "Condition":data["weather"][0]["main"],
+            "Time":data["dt"]
         }
-        with open("weather.json","w") as f:
+        with open("weather.json","a") as f:
             json.dump(weatherinfo,f)
-        with     open("weather.csv","w",newline="") as f:
+        with     open("weather.csv","a",newline="") as f:
             writer=csv.DictWriter(f,fieldnames=weatherinfo.keys())
             writer.writeheader()
             writer.writerow(weatherinfo)
@@ -30,8 +33,57 @@ def get_weather():
     
     else:
         return jsonify({"error":"Failed to fetch weather data"}),500  
-#@app.route("/weather",methods=["POST"])
-#def add():
+@app.route("/weather",methods=["POST"])
+def add():
+    data=request.json
+    cities=[]
+    for i in cities:
+        response=requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={i}&appid={Api_key}")
+        if response.status_code==200:
+            data=response.json()
+            weatherinfo={
+                "city":data["name"],
+                "temperature":data["main"]["temp"],
+                "description":data["weather"][0]["description"],
+                "humidity":data["main"]["humidity"]
+            }
+            cities.append(weatherinfo)
+
+    return jsonify(cities)
+async def fetch_weather(session, city):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={Api_key}"
+    async with session.get(url) as res:
+        if res.status == 200:
+            data = await res.json()
+            return {
+                "city": data["name"],
+                "temperature": data["main"]["temp"],
+                "description": data["weather"][0]["description"],
+                "humidity": data["main"]["humidity"]
+            }
+        
+        
+        else:
+            return {"error": f"Failed to fetch weather data for {city}"}
+async def get_multiple_weather(cities):
+    async with aiohttp.ClientSession() as session:
+        work=[fetch_weather(session, city) for city in cities]
+        return await asyncio.gather(*work)
+@app.route("/weather/many",methods=["POST"])
+def adding():
+    data=request.json
+    cities=data.get("cities",[])
+    ans=asyncio.run(get_multiple_weather(cities))
+    with open("weather.json","w") as f:
+            json.dump(ans,f)
+    with open("weather.csv","w",newline="") as f:
+            writer=csv.DictWriter(f,fieldnames=ans[0].keys())
+            writer.writeheader()
+            for row in ans:
+                writer.writerow(row)
+    return jsonify(ans)
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
